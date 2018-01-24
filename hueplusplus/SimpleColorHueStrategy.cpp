23937b4c964393db7bf029b18d1a497a572ea0a2
Moritz Wirger <lj.moritorii@web.de>
2018-01-24 21:31:10 +0100

    Fix problem in setColorRGB of SimpleColorHueStrategy.cpp - problem was that a rgb color of (0,0,0) is not representable with CIE xy so this case now the light will be turned off

--- hueplusplus/SimpleColorHueStrategy.cpp
+++ hueplusplus/SimpleColorHueStrategy.cpp
@@ -249,6 +249,11 @@ bool SimpleColorHueStrategy::setColorXY(float x, float y, uint8_t transistion, H
 
 bool SimpleColorHueStrategy::setColorRGB(uint8_t r, uint8_t g, uint8_t b, uint8_t transistion, HueLight& light) const
 {
+	if( (r == 0) && (g == 0) && (b == 0) )
+	{
+		return light.OffNoRefresh();
+	}
+
 	const float red = float(r) / 255;
 	const float green = float(g) / 255;
 	const float blue = float(b) / 255;
