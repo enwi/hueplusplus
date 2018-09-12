dd412fe19ce45f25364f7ba6d0dbaea9b0e06bf2
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2018-09-12 14:48:38 +0200

    Fix comment typos.

--- hueplusplus/include/Hue.h
+++ hueplusplus/include/Hue.h
@@ -83,7 +83,7 @@ public:
 
 private:
     //! \brief Normalizes mac address to plain hex number.
-    //! returns \c input without separators and whitespace, in upper case.
+    //! \returns \p input without separators and whitespace, in upper case.
     static std::string NormalizeMac(std::string input);
 
     std::map<std::string, std::string> usernames;		//!< Maps all macs to usernames added by \ref HueFinder::AddUsername
