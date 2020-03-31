09b5ae3802034789c0b9cf3494904d48d6cb3f07
Moritz Wirger <lj.moritorii@web.de>
2020-03-31 22:00:59 +0200

    Change license to GNU LGPLv3 closes #33

--- hueplusplus/HueCommandAPI.cpp
+++ hueplusplus/HueCommandAPI.cpp
@@ -4,17 +4,20 @@
     Copyright (C) 2018  Jan Rogall		- developer\n
     Copyright (C) 2018  Moritz Wirger	- developer\n
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
+    This file is part of hueplusplus.
+
+    hueplusplus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
+
+    hueplusplus is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with hueplusplus.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #include "include/HueCommandAPI.h"
@@ -23,32 +26,35 @@
 
 constexpr std::chrono::steady_clock::duration HueCommandAPI::minDelay;
 
-HueCommandAPI::HueCommandAPI(const std::string &ip, const int port, const std::string &username, std::shared_ptr<const IHttpHandler> httpHandler)
+HueCommandAPI::HueCommandAPI(
+    const std::string& ip, const int port, const std::string& username, std::shared_ptr<const IHttpHandler> httpHandler)
     : ip(ip),
-    port(port),
-    username(username),
-    httpHandler(std::move(httpHandler)),
-    timeout(new TimeoutData{ std::chrono::steady_clock::now(), {} }) {}
+      port(port),
+      username(username),
+      httpHandler(std::move(httpHandler)),
+      timeout(new TimeoutData {std::chrono::steady_clock::now(), {}})
+{}
 
-nlohmann::json HueCommandAPI::PUTRequest(const std::string &path,
-    const nlohmann::json &request) const {
+nlohmann::json HueCommandAPI::PUTRequest(const std::string& path, const nlohmann::json& request) const
+{
     auto now = std::chrono::steady_clock::now();
     std::lock_guard<std::mutex> lock(timeout->mutex);
-    if (timeout->timeout > now) {
+    if (timeout->timeout > now)
+    {
         std::this_thread::sleep_until(timeout->timeout);
     }
     // If path does not begin with '/', insert it unless path is empty
-    const std::string combinedPath =
-        "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") +
-        path;
-    try {
+    const std::string combinedPath = "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") + path;
+    try
+    {
         nlohmann::json v = httpHandler->PUTJson(combinedPath, request, ip);
         timeout->timeout = now + minDelay;
         return v;
     }
-    catch (const std::system_error &e) {
-        if (e.code() == std::errc::connection_reset ||
-            e.code() == std::errc::timed_out) {
+    catch (const std::system_error& e)
+    {
+        if (e.code() == std::errc::connection_reset || e.code() == std::errc::timed_out)
+        {
             // Happens when hue is too busy, wait and try again (once)
             std::this_thread::sleep_for(minDelay);
             nlohmann::json v = httpHandler->PUTJson(combinedPath, request, ip);
@@ -60,25 +66,26 @@ nlohmann::json HueCommandAPI::PUTRequest(const std::string &path,
     }
 }
 
-nlohmann::json HueCommandAPI::GETRequest(const std::string &path,
-    const nlohmann::json &request) const {
+nlohmann::json HueCommandAPI::GETRequest(const std::string& path, const nlohmann::json& request) const
+{
     auto now = std::chrono::steady_clock::now();
     std::lock_guard<std::mutex> lock(timeout->mutex);
-    if (timeout->timeout > now) {
+    if (timeout->timeout > now)
+    {
         std::this_thread::sleep_until(timeout->timeout);
     }
     // If path does not begin with '/', insert it unless path is empty
-    const std::string combinedPath =
-        "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") +
-        path;
-    try {
+    const std::string combinedPath = "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") + path;
+    try
+    {
         nlohmann::json v = httpHandler->GETJson(combinedPath, request, ip);
         timeout->timeout = now + minDelay;
         return v;
     }
-    catch (const std::system_error &e) {
-        if (e.code() == std::errc::connection_reset ||
-            e.code() == std::errc::timed_out) {
+    catch (const std::system_error& e)
+    {
+        if (e.code() == std::errc::connection_reset || e.code() == std::errc::timed_out)
+        {
             // Happens when hue is too busy, wait and try again (once)
             std::this_thread::sleep_for(minDelay);
             nlohmann::json v = httpHandler->GETJson(combinedPath, request, ip);
@@ -90,26 +97,26 @@ nlohmann::json HueCommandAPI::GETRequest(const std::string &path,
     }
 }
 
-nlohmann::json
-HueCommandAPI::DELETERequest(const std::string &path,
-    const nlohmann::json &request) const {
+nlohmann::json HueCommandAPI::DELETERequest(const std::string& path, const nlohmann::json& request) const
+{
     auto now = std::chrono::steady_clock::now();
     std::lock_guard<std::mutex> lock(timeout->mutex);
-    if (timeout->timeout > now) {
+    if (timeout->timeout > now)
+    {
         std::this_thread::sleep_until(timeout->timeout);
     }
     // If path does not begin with '/', insert it unless path is empty
-    const std::string combinedPath =
-        "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") +
-        path;
-    try {
+    const std::string combinedPath = "/api/" + username + (path.empty() || path.front() == '/' ? "" : "/") + path;
+    try
+    {
         nlohmann::json v = httpHandler->DELETEJson(combinedPath, request, ip);
         timeout->timeout = now + minDelay;
         return v;
     }
-    catch (const std::system_error &e) {
-        if (e.code() == std::errc::connection_reset ||
-            e.code() == std::errc::timed_out) {
+    catch (const std::system_error& e)
+    {
+        if (e.code() == std::errc::connection_reset || e.code() == std::errc::timed_out)
+        {
             // Happens when hue is too busy, wait and try again (once)
             std::this_thread::sleep_for(minDelay);
             nlohmann::json v = httpHandler->DELETEJson(combinedPath, request, ip);
--- hueplusplus/test/mocks/mock_HueLight.h
+++ hueplusplus/test/mocks/mock_HueLight.h
@@ -4,17 +4,20 @@
     Copyright (C) 2017  Jan Rogall		- developer\n
     Copyright (C) 2017  Moritz Wirger	- developer\n
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
+    This file is part of hueplusplus.
+
+    hueplusplus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
+
+    hueplusplus is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with hueplusplus.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #ifndef _MOCK_HUE_LIGHT_H
@@ -29,13 +32,12 @@
 #include "../hueplusplus/include/json/json.hpp"
 #include "../testhelper.h"
 
-
 //! Mock Class
 class MockHueLight : public HueLight
 {
 public:
-    MockHueLight(std::shared_ptr<const IHttpHandler> handler) :
-        HueLight(1, HueCommandAPI(getBridgeIp(), getBridgePort(), getBridgeUsername(), handler)) {};
+    MockHueLight(std::shared_ptr<const IHttpHandler> handler)
+        : HueLight(1, HueCommandAPI(getBridgeIp(), getBridgePort(), getBridgeUsername(), handler)) {};
 
     nlohmann::json& getState() { return state; };
 
