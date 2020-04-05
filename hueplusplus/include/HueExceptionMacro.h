90a80dbc27af0b0f3b89966c67dcacbffd9ce746
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2020-04-05 18:45:19 +0200

    Add documentatio to exceptions.

--- hueplusplus/include/HueExceptionMacro.h
+++ hueplusplus/include/HueExceptionMacro.h
@@ -4,20 +4,20 @@
     Copyright (C) 2020  Jan Rogall		- developer\n
     Copyright (C) 2020  Moritz Wirger	- developer\n
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    This file is part of hueplusplus.
 
-    This file contains a macro definition for getting the current line number.
-    It should only be included in cpp files and not in headers!
+    hueplusplus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    hueplusplus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with hueplusplus.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #include "HueException.h"
