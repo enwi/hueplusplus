16b1f51a406f6143c6de82bd837cf1f3a6136e56
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2020-06-27 23:32:08 +0200

    Implement generic sensor.

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -21,54 +21,168 @@
 
 #include "hueplusplus/Sensor.h"
 
+#include "hueplusplus/HueExceptionMacro.h"
+#include "hueplusplus/Utils.h"
 #include "json/json.hpp"
 
 namespace hueplusplus
 {
-int Sensor::getButtonEvent()
+bool Sensor::hasOn() const
 {
-    if (hasButtonEvent())
+    return state.getValue().at("config").count("on") != 0;
+}
+
+bool Sensor::isOn() const
+{
+    return state.getValue().at("config").at("on").get<bool>();
+}
+
+void Sensor::setOn(bool on)
+{
+    sendPutRequest("/config", {"on", on}, CURRENT_FILE_INFO);
+}
+
+bool Sensor::hasBatteryState() const
+{
+    return state.getValue().at("config").count("battery") != 0;
+}
+int Sensor::getBatteryState() const
+{
+    return state.getValue().at("config").at("battery").get<int>();
+}
+void Sensor::setBatteryState(int percent)
+{
+    sendPutRequest("/config", nlohmann::json {{"battery", percent}}, CURRENT_FILE_INFO);
+}
+bool Sensor::hasAlert() const
+{
+    return state.getValue().at("config").count("alert") != 0;
+}
+Alert Sensor::getLastAlert() const
+{
+    std::string alert = state.getValue().at("config").at("alert").get<std::string>();
+    if (alert == "select")
     {
-        return state.getValue().at("state").at("buttonevent");
+        return Alert::select;
     }
-    return 0;
-}
-
-int Sensor::getButtonEvent() const
-{
-    if (hasButtonEvent())
+    else if (alert == "lselect")
     {
-        return state.getValue().at("state").at("buttonevent");
+        return Alert::lselect;
     }
-    return 0;
-}
-
-int Sensor::getStatus()
-{
-    if (hasStatus())
+    else
     {
-        return state.getValue().at("state").at("status");
+        return Alert::none;
     }
-    return 0;
 }
-
-int Sensor::getStatus() const
+void Sensor::sendAlert(Alert type)
 {
-    if (hasStatus())
+    std::string alertStr;
+    switch (type)
     {
-        return state.getValue().at("state").at("status");
+    case Alert::lselect:
+        alertStr = "lselect";
+        break;
+    case Alert::select:
+        alertStr = "select";
+        break;
+    default:
+        alertStr = "none";
+        break;
     }
-    return 0;
+    sendPutRequest("/state", nlohmann::json {{"alert", alertStr}}, CURRENT_FILE_INFO);
+}
+bool Sensor::hasReachable() const
+{
+    return state.getValue().at("config").count("reachable") != 0;
+}
+bool Sensor::isReachable() const
+{
+    // If not present, always assume it is reachable (for daylight sensor)
+    return state.getValue().at("config").value("reachable", true);
 }
 
-bool Sensor::hasButtonEvent() const
+time::AbsoluteTime Sensor::getLastUpdated() const
 {
-    return state.getValue().at("state").count("buttonevent") != 0;
+    const nlohmann::json& stateJson = state.getValue().at("state");
+    auto it = stateJson.find("lastupdated");
+    if (it == stateJson.end() || !it->is_string() || *it == "none")
+    {
+        return time::AbsoluteTime(std::chrono::system_clock::time_point(std::chrono::seconds {0}));
+    }
+    return time::AbsoluteTime::parseUTC(it->get<std::string>());
 }
 
-bool Sensor::hasStatus() const
+bool Sensor::hasUserTest() const
 {
-    return state.getValue().at("state").count("status") != 0;
+    return state.getValue().at("config").count("usertest") != 0;
+}
+void Sensor::setUserTest(bool enabled)
+{
+    sendPutRequest("/config", nlohmann::json {{"usertest", enabled}}, CURRENT_FILE_INFO);
+}
+
+bool Sensor::hasURL() const
+{
+    return state.getValue().at("config").count("url") != 0;
+}
+std::string Sensor::getURL() const
+{
+    return state.getValue().at("config").at("url").get<std::string>();
+}
+void Sensor::setURL(const std::string& url)
+{
+    sendPutRequest("/config", nlohmann::json {{"url", url}}, CURRENT_FILE_INFO);
+}
+
+std::vector<std::string> Sensor::getPendingConfig() const
+{
+    const nlohmann::json& config = state.getValue().at("config");
+    const auto pendingIt = config.find("pending");
+    if (pendingIt == config.end() || !pendingIt->is_array())
+    {
+        return {};
+    }
+    std::vector<std::string> result;
+    result.reserve(pendingIt->size());
+    for (const nlohmann::json& pending : *pendingIt)
+    {
+        result.push_back(pending.get<std::string>());
+    }
+    return result;
+}
+
+bool Sensor::hasLEDIndication() const
+{
+    return state.getValue().at("config").count("ledindication") != 0;
+}
+bool Sensor::getLEDIndication() const
+{
+    return state.getValue().at("config").at("ledindication").get<bool>();
+}
+void Sensor::setLEDIndication(bool on)
+{
+    sendPutRequest("/config", nlohmann::json {{"ledindication", on}}, CURRENT_FILE_INFO);
+}
+
+nlohmann::json Sensor::getState() const
+{
+    return state.getValue().at("state");
+}
+void Sensor::setStateAttribute(const std::string& key, const nlohmann::json& value)
+{
+    sendPutRequest("/state", nlohmann::json {{"key", value}}, CURRENT_FILE_INFO);
+}
+
+bool Sensor::isCertified() const
+{
+    nlohmann::json certified = utils::safeGetMember(state.getValue(), "capabilities", "certified");
+    return certified.is_boolean() && certified.get<bool>();
+}
+
+bool Sensor::isPrimary() const
+{
+    nlohmann::json primary = utils::safeGetMember(state.getValue(), "capabilities", "primary");
+    return primary.is_boolean() && primary.get<bool>();
 }
 
 Sensor::Sensor(int id, const HueCommandAPI& commands, std::chrono::steady_clock::duration refreshDuration)
