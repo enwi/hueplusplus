20bfe58f88be7d4f4a49c41b94418660f89e6c30
Adam Honse <calcprogrammer1@gmail.com>
2020-11-06 10:09:15 -0600

    Add functionality to request client key from Hue bridge and be able to load the client key with the username

--- include/hueplusplus/Bridge.h
+++ include/hueplusplus/Bridge.h
@@ -99,6 +99,12 @@ public:
     //! \param username Username that is used to control the Hue bridge
     void AddUsername(const std::string& mac, const std::string& username);
 
+    //! \brief Function that adds a client key to the clientkeys map
+    //!
+    //! \param mac MAC address of Hue bridge
+    //! \param clientkey Client key that is used to control the Hue bridge in entertainment mode
+    void AddClientKey(const std::string& mac, const std::string& clientkey);
+
     //! \brief Function that returns a map of mac addresses and usernames.
     //!
     //! Note these should be saved at the end and re-loaded with \ref AddUsername
@@ -120,6 +126,8 @@ private:
 
     std::map<std::string, std::string> usernames; //!< Maps all macs to usernames added by \ref
                                                   //!< BridgeFinder::AddUsername
+    std::map<std::string, std::string> clientkeys; //!< Maps all macs to clientkeys added by \ref
+                                                   //!< BridgeFinder::AddClientKey
     std::shared_ptr<const IHttpHandler> http_handler;
 };
 
@@ -145,12 +153,12 @@ public:
     //! \param username String that specifies the username that is used to control
     //! the bridge. Can be left empty and acquired in \ref requestUsername.
     //! \param handler HttpHandler for communication with the bridge
+    //! \param clientkey Optional client key for streaming
     //! \param refreshDuration Time between refreshing the cached state.
     //! \param sharedState Uses a single, shared cache for all objects on the bridge.
     Bridge(const std::string& ip, const int port, const std::string& username,
-        std::shared_ptr<const IHttpHandler> handler,
-        std::chrono::steady_clock::duration refreshDuration = std::chrono::seconds(10),
-        bool sharedState = false);
+        std::shared_ptr<const IHttpHandler> handler, const std::string& clientkey = "",
+        std::chrono::steady_clock::duration refreshDuration = std::chrono::seconds(10), bool sharedState = false);
 
     //! \brief Refreshes the bridge state.
     //!
@@ -192,6 +200,11 @@ public:
     //!
     //! \return The username used for API access
     std::string getUsername() const;
+
+    //! \brief Function that returns the client key
+    //!
+    //! \return The client key used for Entertainment Mode API access
+    std::string getClientKey() const;
 
     //! \brief Function to set the ip address of this class representing a bridge
     //!
@@ -245,6 +258,7 @@ public:
     //! \brief Provides access to the Rule%s on the bridge
     //! \note Does not refresh state.
     const RuleList& rules() const;
+
 private:
     //! \brief Function that sets the HttpHandler and updates the HueCommandAPI.
     //! \param handler a HttpHandler of type \ref IHttpHandler
@@ -259,6 +273,7 @@ private:
     std::string ip; //!< IP-Address of the hue bridge in dotted decimal notation
                     //!< like "192.168.2.1"
     std::string username; //!< Username that is ussed to access the hue bridge
+    std::string clientkey; //!< Client key that is used for entertainment mode
     int port;
 
     std::shared_ptr<const IHttpHandler> http_handler; //!< A IHttpHandler that is used to communicate with the
