3f922e8b154063857a0e2a4490b8467d8f67ff53
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2021-03-11 19:31:55 +0100

    Add an example to read a config file for the username.

--- examples/BridgeSetup.cpp
+++ examples/BridgeSetup.cpp
@@ -1,3 +1,27 @@
+/**
+    \file BridgeSetup.cpp
+    Copyright Notice\n
+    Copyright (C) 2021  Jan Rogall		- developer\n
+
+    This file is part of hueplusplus.
+
+    hueplusplus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    hueplusplus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with hueplusplus.  If not, see <http://www.gnu.org/licenses/>.
+
+    \brief This example connects to a bridge with hardcoded mac and username.
+**/
+
+
 #include <algorithm>
 #include <iostream>
 
@@ -21,6 +45,7 @@ namespace hue = hueplusplus;
 const std::string macAddress = "";
 const std::string username = "";
 
+//! \brief Connects to a bridge and returns it.
 hue::Bridge connectToBridge()
 {
     hue::BridgeFinder finder(std::make_shared<SystemHttpHandler>());
